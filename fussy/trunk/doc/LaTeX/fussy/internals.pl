3ebdf128e40ff42ff0b4b127da19cd5ed4f0402e
Sanjay <sbhatnag@nrao.edu>
2015-10-01 13:36:50 -0600

    New HTML and PDF files

---
+++ fussy/trunk/doc/LaTeX/fussy/internals.pl
@@ -0,0 +1,106 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/DFUNCTIONAL/;
+$ref_files{$key} = "$dir".q|node1.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Tsukanov2003Dsa/;
+$ref_files{$key} = "$dir".q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/EX1/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ScComp/;
+$ref_files{$key} = "$dir".q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/UNITS/;
+$ref_files{$key} = "$dir".q|node16.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Griewank:1996:AAP/;
+$ref_files{$key} = "$dir".q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/APPEN:SUBEXPRESSIONS/;
+$ref_files{$key} = "$dir".q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/APPEN:SYNTAX_FUNC/;
+$ref_files{$key} = "$dir".q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_EDA/;
+$ref_files{$key} = "$dir".q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Stoutemyer:1977/;
+$ref_files{$key} = "$dir".q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_REDUCE/;
+$ref_files{$key} = "$dir".q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/SEC:SINGLE_VAR/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/EX2/;
+$ref_files{$key} = "$dir".q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Bischof1997A-A/;
+$ref_files{$key} = "$dir".q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/FORMATTING/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/APPEN:SYNTAX/;
+$ref_files{$key} = "$dir".q|node14.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/APPEN:EX/;
+$ref_files{$key} = "$dir".q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/APPEN:SYNTAX_EXPR/;
+$ref_files{$key} = "$dir".q|node18.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/FERR/;
+$ref_files{$key} = "$dir".q|node1.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/NUMBERS/;
+$ref_files{$key} = "$dir".q|node15.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Calc/;
+$ref_files{$key} = "$dir".q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_REDUCE2/;
+$ref_files{$key} = "$dir".q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/PRINT/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/SEC:MULTI_VAR/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/SEC:EXAMPLES/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
