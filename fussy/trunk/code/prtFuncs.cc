195d425e952fc53a5e3dd38a42fd38f925c466f0
Sanjay Bhatnagar <sbhatnag@nrao.edu>
2017-12-14 16:07:03 -0700

    Copyright update will happen on if necessary

--- fussy/trunk/code/prtFuncs.cc
+++ fussy/trunk/code/prtFuncs.cc
@@ -1,6 +1,6 @@
 // $Id$
 /******************************************************************
- * Copyright (c) 2000-2010, 2011 S.Bhatnagar
+ * Copyright (c) 2000-2016, 2017 S.Bhatnagar
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
