eaf80118ca129983a8fea45f8526cb0eb388aac5
Sanjay Bhatnagar <sbhatnag@nrao.edu>
2018-11-29 21:21:04 -0700

    Clean up vcode.cc

--- fussy/trunk/code/vcode.cc
+++ fussy/trunk/code/vcode.cc
@@ -315,9 +315,10 @@ void ClearDS(StackType &d)
 //
 void ClearIDL(IDList &d) 
 {
-  IDList::iterator iterend=d.end();
-  //  for(IDList::iterator i=d.begin();i!=d.end();i++) d.erase(*i);
-  for(IDList::iterator i=d.begin();i!=iterend;++i) d.erase(*i);
+  d.clear();
+  // IDList::iterator iterend=d.end();
+  // //  for(IDList::iterator i=d.begin();i!=d.end();i++) d.erase(*i);
+  // for(IDList::iterator i=d.begin();i!=iterend;++i) d.erase(*i);
 }
 //
 //-------------------------------------------------------------------
